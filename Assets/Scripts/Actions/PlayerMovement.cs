002ce7e278e51a4a8fabee2a252360e9fd953894
Cocomineral <tintinch59@gmail.com>
2023-11-14 22:35:18 +0100

    [ADD] Player limit borders

--- Assets/Scripts/Actions/PlayerMovement.cs
+++ Assets/Scripts/Actions/PlayerMovement.cs
@@ -7,16 +7,37 @@ namespace Durian.Actions
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField, BoxGroup("Dependencies")] private Rigidbody2D _rb;
+        [SerializeField, BoxGroup("Dependencies")] private Transform _playerTransform;
 
         [SerializeField, BoxGroup("Setup")] private float _speed;
         [SerializeField, BoxGroup("Setup")] private float _maxSpeed;
 
         private Vector2 _read;
+        private Vector3 leftEdge;
+        private Vector3 rightEdge;
+
+        private void Start()
+        {
+            leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+            rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        }
 
         private void FixedUpdate()
         {
             if(_read == Vector2.zero)
                 _rb.velocity = Vector2.zero;
+
+            if (_read == Vector2.right && _playerTransform.position.x >= rightEdge.x)
+            {
+                _rb.velocity = Vector2.zero;
+                return;
+            }
+
+            if (_read == Vector2.left & _playerTransform.position.x <= leftEdge.x)
+            {
+                _rb.velocity = Vector2.zero;
+                return;
+            }
 
             _rb.AddForce(_read * _speed * Time.fixedDeltaTime, ForceMode2D.Force);
         }
